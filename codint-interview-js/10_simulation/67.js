7867076fd0d6cdce06ad790ebd7661a87594564e
LEE MYEONGUK <muk525@icloud.com>
2026-01-22 21:32:46 +0900

    feat: 문제 67

---
+++ codint-interview-js/10_simulation/67.js
@@ -0,0 +1,17 @@
+function solution(brown, yellow) {
+  const n = brown + yellow
+
+  for(let i = 0; i < n + 1; i++) {
+    // 세로 길이 i
+    if(n % i === 0){
+      // 가로 길이 j
+      let j = Math.floor(n / i);
+      // 갈색 테두리 1줄 배치 개수
+      const b = 2 * j + 2 * i - 4;
+      // 전체 개수 - 갈색 테두리 가 노란색 개수이 때
+      if((n - b) === yellow){
+        return [j, i];
+      }
+    }
+  }
+}
